88b9a89798b8263be11e6e8ea9ad6528b6f91075
Pedro <ussanezio@hotmail.com>
2022-07-27 04:08:50 -0300

    Analise de dados com panda

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+#ler a tabela
+tabela = pd.read_csv("telecom_users.csv")
+
+#remover informações inuteis
+tabela = tabela.drop("Unnamed: 0", axis=1,)
+
+#Trocar coluna de string pra int/float
+tabela["TotalGasto"] = pd.to_numeric(tabela["TotalGasto"], errors='coerce')
+
+#remover clientes com informações faltando
+tabela = tabela.dropna(how="all", axis=1)
+tabela = tabela.dropna(how="any", axis=0)
+
+#Descobrir quantidade de pessoas que cancelaram o plano, (Normalize=True recebe a informação em porcetagem, sem Normalize=True recebe a informação em quantidade)
+print(tabela["Churn"].value_counts(normalize=True))
+
