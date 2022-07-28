7a3a9ec85d42d802bc73fab5e6f639071d6fe68f
Pedro <ussanezio@hotmail.com>
2022-07-28 16:37:59 -0300

    resultado em formato de graficos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+
+import plotly.express as px
 import pandas as pd
-import re
 
 numero = []
 
@@ -16,6 +17,5 @@ tabela["TotalGasto"] = pd.to_numeric(tabela["TotalGasto"], errors='coerce')
 tabela = tabela.dropna(how="all", axis=1)
 tabela = tabela.dropna(how="any", axis=0)
 
-#Descobrir quantidade de pessoas que cancelaram o plano, (Normalize=True recebe a informação em porcetagem, sem Normalize=True recebe a informação em quantidade)
-print(tabela["Churn"].value_counts())
-print(tabela["Churn"].value_counts(normalize=True).map("{:.2%}".format))
+grafico = px.histogram(tabela, x="MesesComoCliente", color="Churn", text_auto=True)
+grafico.show()
