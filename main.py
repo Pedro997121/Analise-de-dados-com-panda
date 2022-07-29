361abd6825cef9d76f242868b229593483e94c53
Pedro <ussanezio@hotmail.com>
2022-07-28 23:16:25 -0300

    .info()

--- main.py
+++ main.py
@@ -17,5 +17,9 @@ tabela["TotalGasto"] = pd.to_numeric(tabela["TotalGasto"], errors='coerce')
 tabela = tabela.dropna(how="all", axis=1)
 tabela = tabela.dropna(how="any", axis=0)
 
+#informação sobre a tabela
+print(tabela.info())
+
+#resultado em forma de grafico
 grafico = px.histogram(tabela, x="MesesComoCliente", color="Churn", text_auto=True)
 grafico.show()
